983a4afc4585ca7e0dcd40ec3e9d3832ae37283d
Vortm4x <maksym.trehubenko@nure.ua>
2021-02-21 14:26:34 +0200

    class List commented out, struct Node<T> realised

--- main.cpp
+++ main.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
 
 template<typename T>
+struct Node
+{
+    Node<T>* next;
+    T data;
+
+    Node();
+    Node(T data, Node<T>* next);
+};
+/*
 class List
 {
 private:
     int count;
-    Node* head;
+    Node<T>* head;
 
 public:
+    List();
 
 };
 
-struct Node
+*/
+
+template<typename T>
+Node<T>::Node()
 {
+        next = nullptr;
+}
 
-
-
-};
-
-
+template<typename T>
+Node<T>::Node(T data, Node<T>* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
 
 int main()
 {
-
-
-
-
-
-
-
-
-
+    Node<int>* node = new Node<int>(5, nullptr);
+    std::cout << node->data;
 
 
     return 0;
