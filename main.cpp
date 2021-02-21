a33f81b55d00a1abc63085158f04cc25be2ce023
Vortm4x <maksym.trehubenko@nure.ua>
2021-02-21 15:23:02 +0200

    List<T> destructor added, comments added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+// -----------------------------------------
+//  struct Node<T>
+//  Consists data and ptr on next node
+// -----------------------------------------
 template<typename T>
 struct Node
 {
@@ -10,6 +14,11 @@ struct Node
     Node(T data, Node<T>* next);
 };
 
+// -------------------------------------------
+//  class List<T>
+//  Consists  a quantity of nodes and
+//  and a ptr on the next one + methods
+// -------------------------------------------
 template<typename T>
 class List
 {
@@ -19,15 +28,23 @@ private:
 
 public:
     List();
-
+    ~List();
 };
 
+// -------------------------------
+//  Node<T>::Node()
+//  Default node constructor
+// -------------------------------
 template<typename T>
 Node<T>::Node()
 {
         next = nullptr;
 }
 
+// -----------------------------------------------
+//  Node<T>::Node(T data, Node<T>* next)
+//  Node constructor  with params
+// -----------------------------------------------
 template<typename T>
 Node<T>::Node(T data, Node<T>* next)
 {
@@ -35,11 +52,33 @@ Node<T>::Node(T data, Node<T>* next)
         this->next = next;
 }
 
+// -----------------------------
+//  List<T>::List()
+//  Default list constructor
+// -----------------------------
 template<typename T>
 List<T>::List()
 {
     head = nullptr;
     count = 0;
+}
+
+// ----------------------------
+//  List<T>::~List()
+//  Default list destructor
+// ----------------------------
+template<typename T>
+List<T>::~List()
+{
+    count = 0;
+    Node<T>* current;
+
+    while(head != nullptr)
+    {
+        current = head;
+        head = head->next;
+        delete current;
+    }
 }
 
 int main()
