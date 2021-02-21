3881070ceaa838038be4c0bf296cf9d3027e4a87
Vortm4x <maksym.trehubenko@nure.ua>
2021-02-21 13:24:30 +0200

    source file created, class List and struct Node added (empty)

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+template<typename T>
+class List
+{
+private:
+    int count;
+    Node* head;
+
+public:
+
+};
+
+struct Node
+{
+
+
+
+};
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
