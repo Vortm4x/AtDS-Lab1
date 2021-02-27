b976c1651e48eb9518d4dc81cd746cadc4616e04
Vortm4x <maksym.trehubenko@nure.ua>
2021-02-27 21:27:23 +0200

    method int remove(const T& data) added

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ public:
 	T shift();
 	void push_back(const T& data);
 	T pop_back();
+	int remove(const T& data);
 };
 
 // -----------------------------------------------
@@ -218,6 +219,49 @@ T List<T>::pop_back()
 	}
 }
 
+// ----------------------------------------------------
+//  void List<T>::remove()
+//	  Removes all found values as in arguments,
+//  if there are no, does nothing; returns size
+// ----------------------------------------------------
+template<typename T>
+int List<T>::remove(const T & data)
+{
+	if(!isEmpty())
+	{
+		Node<T>* previous = nullptr;
+		Node<T>* current = head;
+
+		do
+		{
+			if (current->data == data)
+			{
+				if (previous == nullptr)
+				{
+					head = head->next;
+					delete current;
+					current = head;
+				}
+				else
+				{
+					previous->next = current->next;
+					delete current;
+					current = previous->next;
+				}
+
+				--count;
+			}
+			else
+			{
+				previous = current;
+				current = current->next;
+			}
+		} while (current != nullptr);
+	}
+
+	return count;
+}
+
 // --------------------------
 //  void List<T>::print()
 //	  Prints the list
@@ -242,19 +286,22 @@ void List<T>::print()
 	}
 }
 
+
+
 int main()
 {
     List<int> numbers;
    
 	for (int i(0); i < 6; ++i)
 	{
-		numbers.push_back(i);
+		numbers.push_back(2);
+		numbers.push_back(1);
 	}
+	numbers.push_back(2);
 
 	numbers.print();
-	std::cout << numbers.shift() << '\n';
+	std::cout << numbers.remove(2) << '\n';
 	numbers.print();
-
 
     return 0;
 }
